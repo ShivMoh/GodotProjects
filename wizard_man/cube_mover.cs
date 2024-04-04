dd69b5c439c4cb00a2f3e319059e56f98f8c8696
ShivMoh <shiveshmohamed1234@gmail.com>
2024-04-04 09:17:33 -0400

    added a new project

---
+++ wizard_man/cube_mover.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class cube_mover : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	[Export]
+	MeshInstance3D cube;
+
+	Vector3 cubeCoords = new Vector3(1.0f, 1.0f, 1.0f);
+	public override void _Ready()
+	{
+		
+		cube = GetNode<MeshInstance3D>("cube");
+		cubeCoords = cube.Position;
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var default_cube = GD.Load<PackedScene>("res://default_cube.tscn");
+
+		default_cube csdfd = default_cube.Instantiate<default_cube>();
+		csdfd.Position = new Vector3(0.0f, 0.0f, 0.0f);
+		AddChild(csdfd);
+		cubeCoords.X += 0.001f;
+		cube.Position = cubeCoords;
+		
+	}
+}
