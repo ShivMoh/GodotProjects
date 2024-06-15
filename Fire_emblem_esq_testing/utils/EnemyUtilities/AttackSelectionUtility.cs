34870f5f2e09f40d985e74088d98ad9a38adf2e1
shivmoh <shiveshmohamed1234@gmail.com>
2024-06-14 22:38:51 -0400

    worked on some stuff

--- Fire_emblem_esq_testing/utils/EnemyUtilities/AttackSelectionUtility.cs
+++ Fire_emblem_esq_testing/utils/EnemyUtilities/AttackSelectionUtility.cs
@@ -1,15 +1,144 @@
 using Godot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public partial class AttackSelectionUtility {
 
-    public static List<AttackMeta> sortAttacksByRange(List<AttackMeta> attackMetas) {
-        return (List<AttackMeta>)attackMetas.OrderByDescending(attack => attack.attackTargetMeta.range);
-    }
+	private EnemyCharacter enemyCharacter;
+	private List<List<Character>> targetCandidates;
 
-    public static List<AttackMeta> getAvailableAttacks(Character character) {
-        if (character.attacks.Count() == 0) return new List<AttackMeta>();
-        return character.attacks.FindAll(attack => attack.timesUsableUntilReset > 0);
-    }
+	private List<Character> targetableCharactersInCloseRange;
+
+	private List<AttackMeta> availableAttacks; 
+
+	private AttackMeta chosenAttack;
+
+	private Character target;
+
+	public AttackSelectionUtility(
+		EnemyCharacter enemyCharacter, 
+		List<List<Character>> targetCandidates,
+		List<AttackMeta> availableAttacks,
+		List<Character> targetableCharactersInCloseRange
+	) {
+		this.enemyCharacter = enemyCharacter;
+		this.targetCandidates = targetCandidates;
+		this.availableAttacks = availableAttacks;
+		this.targetableCharactersInCloseRange = targetableCharactersInCloseRange;
+	}
+
+	public void chooseCunningCharacterAttack() {
+	  
+		List<AttackMeta> attackCandidates = new List<AttackMeta>();
+		List<Character> refinedTargetCandidates = new List<Character>();        
+
+		if (canAttackFromDistance()) {
+			attackCandidates = chooseAttacksWithGreatestRange();
+			
+			for (int i = 0; i < attackCandidates.Count(); i++)
+			{
+				List<Character> characterList = targetCandidates.ElementAt(i);
+				
+				foreach (Character character in characterList)
+				{
+					if (!willAttackingHurtCharacter(character, attackCandidates.ElementAt(i))) {
+						refinedTargetCandidates.Add(character);
+					}
+				}
+			}
+		}
+
+
+		if (refinedTargetCandidates.Count() == 0) {
+			List<AttackMeta> closeRangeAttacks = this.getCloseRangeAttacks();
+			List<Character> closeRangeTargetable = new List<Character>();
+
+			foreach (Character character in targetableCharactersInCloseRange)
+			{
+				if (closeRangeAttacks.Count() > 0) {
+					if (!willAttackingHurtCharacter(character, closeRangeAttacks.First())) {
+						closeRangeTargetable.Add(character);
+					}
+				}     
+			}
+
+			if (closeRangeTargetable.Count() > 0) {
+				refinedTargetCandidates = closeRangeTargetable;
+				attackCandidates = closeRangeAttacks;
+			} else {
+				refinedTargetCandidates = this.targetCandidates.ElementAt(0);
+			}
+		} else {
+			// do nothing ig
+		}
+		
+		this.chosenAttack = this.choseRandomAttack(attackCandidates);
+		this.target = this.chooseRandomCharacter(refinedTargetCandidates);
+	}
+
+	public Character getTarget() {
+		return this.target;
+	}
+
+	public AttackMeta getAttack() {
+		return this.chosenAttack;
+	}
+
+	private AttackMeta choseRandomAttack(List<AttackMeta> attacks) {
+		var rand = new Random();
+
+		int randomIndex = rand.Next(0, attacks.Count());
+
+		return attacks.ElementAt(randomIndex);
+	}
+
+	private Character chooseRandomCharacter(List<Character> characters) {
+		var rand = new Random();
+
+		int randomIndex = rand.Next(0, characters.Count());
+
+		return characters.ElementAt(randomIndex);
+	}
+
+	private bool canAttackFromDistance() {
+		return this.availableAttacks.First().attackTargetMeta.range > 1;
+	}
+
+	private List<AttackMeta> getCloseRangeAttacks() {
+		return (List<AttackMeta>)   this.availableAttacks
+									.Where(attack => attack.attackTargetMeta.closeRange == true)
+									.ToList();
+	}
+
+	private List<AttackMeta> chooseAttacksWithGreatestRange() {
+		int greatestRange = availableAttacks.Max(attack => attack.attackTargetMeta.range);
+		return (List<AttackMeta>)   availableAttacks
+									.Where(attack => attack.attackTargetMeta.range == greatestRange)
+									.OrderByDescending(attack => attack.attackTargetMeta.range)
+									.ToList();
+	}
+
+	private bool willAttackingHurtCharacter(Character target, AttackMeta attackCandidate) {
+		return target.equipedAttack.attackTargetMeta.range >= attackCandidate.attackTargetMeta.range;
+	}
+
+	private bool shouldEnemyAttackFromDistance() {
+
+		if (enemyCharacter.characterStat.health > 10) {
+			return false;
+		}        
+
+		return true;
+	}
+	public static List<AttackMeta> sortAttacksByRange(List<AttackMeta> attackMetas) {
+		return (List<AttackMeta>)   attackMetas
+									.OrderByDescending(attack => attack.attackTargetMeta.range)
+									.ToList();
+	}
+
+	public static List<AttackMeta> determineAvailableAttacks(Character character) {
+		if (character.attacks.Count() == 0) return new List<AttackMeta>();
+		return character.attacks.FindAll(attack => attack.timesUsableUntilReset > 0);
+	}
 }
--- Fire_emblem_esq_testing/utils/EnemyUtilities/EnemySelectionUtillity.cs
+++ Fire_emblem_esq_testing/utils/EnemyUtilities/EnemySelectionUtillity.cs
@@ -4,101 +4,106 @@ using System.Linq;
 using Godot;
 
 public partial class EnemySelectionUtility {
-    private List<PlayableCharacter> playableCharacters;
+	private List<PlayableCharacter> playableCharacters;
 
-    private EnemyCharacter enemyCharacter;
+	private EnemyCharacter enemyCharacter;
 
-    private TileMap tileMap;
+	private TileMap tileMap;
 
-    private List<AttackMeta> availableAttacks;
+	private List<AttackMeta> availableAttacks;
 
-    public EnemySelectionUtility(
-        EnemyCharacter enemyCharacter,
-        TileMap tileMap
-    ) {
-        this.enemyCharacter = enemyCharacter;
-        this.tileMap = tileMap;
-    }
+	public EnemySelectionUtility(
+		EnemyCharacter enemyCharacter,
+		TileMap tileMap
+	) {
+		this.enemyCharacter = enemyCharacter;
+		this.tileMap = tileMap;
+	}
 
-    /*
-        ["Fire ball", "Ice ball"]
-        [
-            ["this guy", "that guy"],
-            ["this guy", "that guy"]
-        ]
+	/*
+		["Fire ball", "Ice ball"]
+		[
+			["this guy", "that guy"],
+			["this guy", "that guy"]
+		]
 
-        // get all targetable for fireball
+		// get all targetable for fireball
 
-        fireballindex = indexof(fireball)
-        list[fireballindex]
-            */
+		fireballindex = indexof(fireball)
+		list[fireballindex]
+			*/
+
+	public List<AttackMeta> getAvailableAttacks() {
+		return this.availableAttacks;
+	}
 	public List<List<Character>> findTargetsWithinRange(List<PlayableCharacter> characters) {
 
-        Vector2I enemyTileLocation = tileMap.LocalToMap(enemyCharacter.GlobalPosition);
-        List<AttackMeta> availableAttacks = (List<AttackMeta>) AttackSelectionUtility.getAvailableAttacks(enemyCharacter).OrderByDescending(attack => attack.attackTargetMeta.range);
-        List<List<Character>> targetCandidates = new List<List<Character>>();
+		Vector2I enemyTileLocation = tileMap.LocalToMap(enemyCharacter.GlobalPosition);
+		this.availableAttacks = AttackSelectionUtility.determineAvailableAttacks(enemyCharacter);
+		//.OrderByDescending(attack => attack.attackTargetMeta.range);
+		List<List<Character>> targetCandidates = new List<List<Character>>();
 
-        if (availableAttacks.Count() == 0) return targetCandidates; 
-        foreach (AttackMeta attack in availableAttacks) {
-            List<Character> targetableCandidatesForAttack = new List<Character>();
+		if (availableAttacks.Count() == 0) return targetCandidates; 
+		foreach (AttackMeta attack in availableAttacks) {
+			List<Character> targetableCandidatesForAttack = new List<Character>();
 
-		    foreach (PlayableCharacter playableCharacter in characters) {
-                Vector2I playableTileLocation = tileMap.LocalToMap(playableCharacter.GlobalPosition);
+			foreach (PlayableCharacter playableCharacter in characters) {
+				Vector2I playableTileLocation = tileMap.LocalToMap(playableCharacter.GlobalPosition);
 
-                int numberOfStepsTo = Mathf.Abs(playableTileLocation.Y - enemyTileLocation.Y) + 
-                Mathf.Abs(playableTileLocation.X - enemyTileLocation.X) - (attack.attackTargetMeta.range + 1);
+				int numberOfStepsTo = Mathf.Abs(playableTileLocation.Y - enemyTileLocation.Y) + 
+				Mathf.Abs(playableTileLocation.X - enemyTileLocation.X) - (attack.attackTargetMeta.range + 1);
 
-                if (numberOfStepsTo <= enemyCharacter.moveSteps) {
-                    targetableCandidatesForAttack.Add(playableCharacter);
-                }          
+				if (numberOfStepsTo <= enemyCharacter.moveSteps) {
+					targetableCandidatesForAttack.Add(playableCharacter);
+				}          
 
-                
-            }          
-            
-            targetCandidates.Add(targetableCandidatesForAttack);
+				
+			}          
+			
+			targetCandidates.Add(targetableCandidatesForAttack);
 
 		}
 
-        return targetCandidates;
+		return targetCandidates;
 	}
 
-    public List<Character> findTargetsWithinCloseRange(List<Character> characters) {
-        List<Character> targetCandidates = new List<Character>();
-        Vector2I enemyTileLocation = tileMap.LocalToMap(enemyCharacter.GlobalPosition);
+	public List<Character> findTargetsWithinCloseRange(List<PlayableCharacter> characters) {
+		List<Character> targetCandidates = new List<Character>();
+		Vector2I enemyTileLocation = tileMap.LocalToMap(enemyCharacter.GlobalPosition);
 
-        foreach (PlayableCharacter playableCharacter in characters) {
-            Vector2I playableTileLocation = tileMap.LocalToMap(playableCharacter.GlobalPosition);
+		foreach (PlayableCharacter playableCharacter in characters) {
+			Vector2I playableTileLocation = tileMap.LocalToMap(playableCharacter.GlobalPosition);
 
-            int numberOfStepsTo = Mathf.Abs(playableTileLocation.Y - enemyTileLocation.Y) + 
-            Mathf.Abs(playableTileLocation.X - enemyTileLocation.X) - 1;
+			int numberOfStepsTo = Mathf.Abs(playableTileLocation.Y - enemyTileLocation.Y) + 
+			Mathf.Abs(playableTileLocation.X - enemyTileLocation.X) - 1;
 
-            if (numberOfStepsTo <= enemyCharacter.moveSteps) {
-                targetCandidates.Add(playableCharacter);
-            }        
-        }  
+			if (numberOfStepsTo <= enemyCharacter.moveSteps) {
+				targetCandidates.Add(playableCharacter);
+			}        
+		}  
 
-        return targetCandidates;
-    } 
+		return targetCandidates;
+	} 
 
  
 
-    public Character findWeakestDefenceTarget(List<Character> characters) {
-        if (enemyCharacter.characterStat.strenth > enemyCharacter.characterStat.magic) {
-            return characters.MinBy(character => character.characterStat.physicalDefence);
-        } else {
-            return characters.MinBy(character => character.characterStat.magicalDefence);
-        }
-    }
+	public Character findWeakestDefenceTarget(List<Character> characters) {
+		if (enemyCharacter.characterStat.strenth > enemyCharacter.characterStat.magic) {
+			return characters.MinBy(character => character.characterStat.physicalDefence);
+		} else {
+			return characters.MinBy(character => character.characterStat.magicalDefence);
+		}
+	}
 
-    public void findStrongestTarget() {}
-    public void findLowestHealthTarget() {}
+	public void findStrongestTarget() {}
+	public void findLowestHealthTarget() {}
 
-    // the area where a given multi attack will have the most effect
-    public void findLargestCharacterPool() {}
-    
-    // like a target u can attack from a distance
-    // or without getting hurt back
-    public void findSafestToAttackTarget() {}
+	// the area where a given multi attack will have the most effect
+	public void findLargestCharacterPool() {}
+	
+	// like a target u can attack from a distance
+	// or without getting hurt back
+	public void findSafestToAttackTarget() {}
 }
 
 
